6ecb5960690593d576ada615e87a271683be7717
Abdulmajed Dhelaimi <adhelaim@gmail.com>
2019-09-18 14:22:00 +0300

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,11 @@
+console.log("Up and running!");
+
+var cardOne = "King of Diamonds";
+var cardTwo = "Queen of Diamonds";
+var cardThree = "King of Hearts";
+var cardFour = "Queen of Hearts";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardFour);
